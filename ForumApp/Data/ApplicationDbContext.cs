0925ffbabdd15163036afda44d6ea476cfa44df1
KoczOo <sebastian.koczotowski@gmail.com>
2019-03-06 21:23:56 +0100

    Add project files.

---
+++ ForumApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ForumApp.Data.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ForumApp.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+        public DbSet<Forum> Forums { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<PostReplay> PostReplays { get; set; }
+
+
+
+    }
+}
---
+++ ForumApp/Data/Models/PostReplay.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ForumApp.Data.Models
+{
+    public class PostReplay
+    {
+        public int Id { get; set; }
+        public string Content { get; set; }
+        public DateTime Creation { get; set; }
+        
+        public virtual ApplicationUser User { get; set; }
+        public virtual Post Post { get; set; }
+    }
+}
